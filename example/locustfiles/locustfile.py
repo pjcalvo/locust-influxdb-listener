c7e28dab07b3b5215654492a197cdf72cf017dcc
pablo <pjcalvov@gmail.com>
2021-12-13 14:18:58 -0600

    radford

--- example/locustfiles/locustfile.py
+++ example/locustfiles/locustfile.py
@@ -1,4 +1,5 @@
 from locust import between, events, tag, task, HttpUser
+import random
 
 from locust_influxdb_listener import InfluxDBListener, InfluxDBSettings
 
@@ -14,30 +15,73 @@ def on_locust_init(environment, **_kwargs):
         influx_port = '8086',
         user = 'admin',
         pwd = 'pass',
-        database = 'test-project'
+        database = 'radford'
     )
     # start listerner with the given configuration
     InfluxDBListener(env=environment, influxDbSettings=influxDBSettings)
 
+urls_important = [
+    'content/admissions/home.html',
+    'content/library.html',
+    'content/radfordcore/home/about.html',
+    'content/radfordcore/home/academics/colleges-and-departments.html',
+    'content/radfordcore/home/academics/courses-and-schedules/calendar.html',
+    'content/ruc/home.html',
+    'content/radfordcore/home/directory.html',
+    'content/radfordcore/home/admissions.html',
+    'content/radfordcore/home/academics.html',
+    'content/radfordcore/home/admissions/apply-now.html',
+    'content/registrar/home/registration-information/academic-calendar.html',
+    'content/admissions/home/new-freshman.html',
+    'content/admissions/home/apply.html',
+    'content/incoming/home.html',
+    'content/residence-life/home.html',
+    'content/grad/home/academics/graduate-programs.html',
+    'content/grad/home.html',
+    'content/residence-life/home/residence-halls.html',
+    'content/radfordcore/home/about/employment.html',
+]
+
+urls_important_less = [
+    'content/radfordcore/home/contact.html',
+    'content/impact.html',
+    'content/president-office/home.html',
+    'content/financial-aid/home.html',
+    'content/human-resources/home.html',
+    'content/cobe/home.html',
+    'content/cehd/home.html',
+    'content/wchs/home.html',
+    'content/chbs/home.html',
+    'content/nursing/home.html',
+    'content/csat/home.html',
+    'content/cvpa/home.html'
+]
+
 class TestWebUser(HttpUser):
 
     wait_time = between(1,5)
-         
+
     @tag('home_page')
-    @task(1)
+    @task(3 )
     def home_page(self):
-        with self.client.get("/", catch_response=True) as response:
+        with self.client.get("/content/radfordcore/home.html", catch_response=True) as response:
             if response.status_code != 200:
                 response.failure("Got wrong response")
     
-    @tag('connectors')
+    @tag('important_urls')
+    @task(10)
+    def workfront_connector(self):
+        with self.client.get(f"/{random.choice(urls_important)}", catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure("Got wrong response")
+
+    @tag('less_important_urls')
     @task(1)
     def workfront_connector(self):
-        with self.client.get("/connectors/workfront", catch_response=True) as response:
-            if 'Do More Work, Faster' not in response.text:
-                response.failure("Expected test was not displayed")
+        with self.client.get(f"/{random.choice(urls_important_less)}", catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure("Got wrong response")
             
     
     def on_start(self):
         print('New user was spawned')
-       
